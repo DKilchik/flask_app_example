22ec911bcf36194de6bae0a221fd2d05ca67f374
DKilchik <dmitrykilchik@gmail.com>
2020-05-26 22:13:59 +0300

    base template edited

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm
 from app.models import Users
@@ -8,6 +8,7 @@ from app.models import Users
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
