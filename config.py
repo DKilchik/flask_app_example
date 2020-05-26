d491cc805640eea91602b7abaeeb3a34f4e0d886
DKilchik <dmitrykilchik@gmail.com>
2020-05-26 13:12:57 +0300

    login and registration adding started

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = env.str("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = env.str("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
