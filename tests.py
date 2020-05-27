5954506a17560d9c292d76e3e2efd783213e518d
DKilchik <dmitrykilchik@gmail.com>
2020-05-27 20:48:57 +0300

    tests.py added

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+from config import Config
+from app import create_app, db
+from app.models import Users
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = Users(username='test')
+        u.set_password('test')
+        self.assertFalse(u.check_password('false'))
+        self.assertTrue(u.check_password('test'))
+
+
+if __name__ == '__main__':
+    unittest.main()
