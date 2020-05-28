32b0b5414ed29440105491067b79c09fa015c210
DKilchik <dmitrykilchik@gmail.com>
2020-05-28 21:27:01 +0300

    subject added

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,12 @@
 from app.main import bp
 from flask_login import login_required
 from flask import render_template
-
-
-
+from app.models import Subjects
 
 
 @bp.route('/')
 @bp.route('/index')
 @login_required
 def index():
-    return render_template('index.html')
+    subjects = Subjects.query.all()
+    return render_template('index.html', subjects=subjects)
--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -23,3 +22,11 @@ class Users(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return Users.query.get(int(id))
+
+
+class Subjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Subject {}>'.format(self.name)
