83a5f6c555cff2f7cc7dd5a21474c49d947b15e2
DKilchik <dmitrykilchik@gmail.com>
2020-05-29 22:33:42 +0300

    route to tests select page added

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from app.main import bp
 from flask_login import login_required
 from flask import render_template
-from app.models import Subjects
+from app.models import Subjects, Tests
 
 
 @bp.route('/')
@@ -9,11 +9,11 @@ from app.models import Subjects
 @login_required
 def index():
     subjects = Subjects.query.all()
-    print(subjects)
     return render_template('index.html', subjects=subjects)
 
 
-@bp.route('subjects/<subject>')
+@bp.route('/subjects/<int:subject_id>')
 @login_required
-def subject_tests(subject):
-    return render_template('subject.html', subject=subject)
+def subject_tests(subject_id):
+    tests = Tests.query.filter_by(subject_id=subject_id).all()
+    return render_template('subject.html', tests=tests)
--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Users(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User ({},{})>'.format(self.id, self.username)
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
